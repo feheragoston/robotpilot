8caf3f3c93cabb5ba0df903bedd8307c4e9edc35
agoston <agoston@2f3a4283-9045-0410-90d5-c73a46827e3c>
2011-03-31 23:25:16 +0000

    lua forditasa ne jelezzen warningot (POSIX hasznalata), lua simulator teszteles
    git-svn-id: file:///var/svn/robute/Eurobot2011/robotpilot@1093 2f3a4283-9045-0410-90d5-c73a46827e3c

--- Pilot/test.lua
+++ Pilot/test.lua
@@ -1,6 +1,9 @@
-repeat ControlWait(); until (Go() ~= 0);
+repeat ControlWait(); until (Go(100) ~= 0);
 Print("Odaertunk");
-repeat ControlWait(); until (Turn(-3.14/2) ~= 0);
+repeat ControlWait(); until (GoTo(250, 1500) ~= 0);
+Print("GoTo finished");
+x, y, phi = GetRobotPos();
+repeat ControlWait(); until (Turn(-phi) ~= 0);
 Print("Forogtunk");
 repeat ControlWait(); until (Go() ~= 0);
 Print("Odaertunk");
