302d277b92f7a3da221f2553e79e9a4c9eeff6a0
triszalo <triszalo@2f3a4283-9045-0410-90d5-c73a46827e3c>
2011-04-23 10:44:05 +0000

    PrimitivesCan-ben a node-azonosítók át lettek írva
    git-svn-id: file:///var/svn/robute/Eurobot2011/robotpilot@1130 2f3a4283-9045-0410-90d5-c73a46827e3c

--- Primitives/nodes/include/node.h
+++ Primitives/nodes/include/node.h
@@ -23,21 +23,20 @@
 
 
 //------------------------------ id ELEJE ------------------------------
-#define PC_ID				0
-#define GATEWAY_ID			1
-#define BROADCAST_ID		2
+#define PC_ID						0
+#define GATEWAY_ID					1
+#define BROADCAST_ID				2
+#define BOOTLOADER_ID				30
 
-#define CONSOLE_ID			3
-#define INPUT_ID			6
-#define SERVO_ID			8
-#define SONAR_ID			9
-#define SUPPLY_ID			10
 
-#define DEADRECK_ID			SERVO_ID
-#define DRIVE_ID			SERVO_ID
-#define MAGNET_ID			CONSOLE_ID
-
-#define BOOTLOADER_ID		30
+#define CONSOLE_ID					3
+#define DEADRECK_ID					4
+#define DRIVE_ID					5
+#define INPUT_ID					6
+#define MAGNET_ID					7
+#define SERVO_ID					8
+#define SONAR_ID					9
+#define SUPPLY_ID					10
 //------------------------------ id VEGE ------------------------------
 
 
