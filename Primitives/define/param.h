bad2247670e29f40e0231947e5328ad79c73754a
Szaloki David <triszalo@freemail.hu>
2011-06-17 21:30:27 +0200

    gripper sebesség és gyorsulás beállítva, hogy ne csorduljon túl, de nagy legyen
    Change-Id: I6d35fe87da956a69db504551111035efc1b1137b

--- Primitives/define/param.h
+++ Primitives/define/param.h
@@ -208,8 +208,8 @@
 
 
 //----- servo -----
-#define SERVO_GRIPPER_MAX_SPEED						360000	//deg/s
-#define SERVO_GRIPPER_MAX_ACC						360000	//deg/s^2
+#define SERVO_GRIPPER_MAX_SPEED						1000	//deg/s
+#define SERVO_GRIPPER_MAX_ACC						1000	//deg/s^2
 
 
 #define SERVO_GRIPPER_LEFT_INDEX						1
