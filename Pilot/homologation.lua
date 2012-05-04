f7348f897b078aa044d5e8bd53a96af412f91f85
Agoston Feher <feher.agoston@gmail.com>
2012-05-04 15:25:54 +0300

    tolatas hiba kezeles, gombhoz tavolabb megyunk
    hogy a cd-be ne akadjunk el

--- Pilot/homologation.lua
+++ Pilot/homologation.lua
@@ -22,18 +22,18 @@ if (c.simulate(p.GoToSafe, 860, 500 * Ori + Offset)) then
 	p.GoToSafe(860, 500 * Ori + Offset)
 end
 if (c.simulate(p.GoSafe, -220)) then
-	p.GoSafe(-220)
+	repeat p.GoSafe(-220) until (c.GetMotionError() == 0)
 end
 
 
-if (c.simulate(p.TurnToSafe, 1775, 640 * Ori + Offset)) then
-	p.TurnToSafe(1775, 640 * Ori + Offset)
-	if (c.simulate(p.GoToSafe, 1775, 640 * Ori + Offset)) then
-		p.GoToSafe(1775, 640 * Ori + Offset)
+if (c.simulate(p.TurnToSafe, 1775, 670 * Ori + Offset)) then
+	p.TurnToSafe(1775, 670 * Ori + Offset)
+	if (c.simulate(p.GoToSafe, 1775, 670 * Ori + Offset)) then
+		p.GoToSafe(1775, 670 * Ori + Offset)
 		p.ClawMove(false, 30)
 		p.ClawMove(false, 2)
 		if (c.simulate(p.GoSafe, -220)) then
-			p.GoSafe(-220)
+			repeat p.GoSafe(-220) until (c.GetMotionError() == 0)
 		end
 	end
 end
@@ -72,7 +72,7 @@ if (c.simulate(p.TurnToSafe, 1700, Offset + Ori * 790)) then
 				p.GripperMove(true, 95)
 				p.GripperMove(false, 95)
 				if (c.simulate(p.GoSafe, -300)) then
-					p.GoSafe(-300)
+					repeat p.GoSafe(-300) until (c.GetMotionError() == 0)
 				end
 				p.GripperMove(true, 0)
 				p.GripperMove(false, 1)
