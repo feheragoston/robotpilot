38754e52ee4816e02997d2e0bc16993492c18865
Agoston Feher <feher.agoston@gmail.com>
2012-05-04 10:26:54 +0200

    homologation biztonsagosabb

--- Pilot/homologation.lua
+++ Pilot/homologation.lua
@@ -14,16 +14,28 @@ p.GripperMove(false, 95)
 p.GripperMove(true, 95)
 p.ClawMove(false, 90)
 p.ClawMove(true, 120)
-p.GoToSafe(1400, Offset + Ori * 800)
+if (c.simulate(p.GoToSafe, 1400, Offset + Ori * 800)) then
+	p.GoToSafe(1400, Offset + Ori * 800)
+end
 p.GripperMove(true, 55)
 p.GripperMove(false, 55)
-p.TurnToSafe(700, Offset + Ori * 700)
-p.GoToSafe(700, Offset + Ori * 700)
-p.TurnToSafe(700, Offset + Ori * 400)
-p.GoToSafe(700, Offset + Ori * 400)
+if (c.simulate(p.TurnToSafe, 700, Offset + Ori * 700)) then
+	p.TurnToSafe(700, Offset + Ori * 700)
+end
+if (c.simulate(p.GoToSafe, 700, Offset + Ori * 700)) then
+	p.GoToSafe(700, Offset + Ori * 700)
+end
+if (c.simulate(p.TurnToSafe, 700, Offset + Ori * 400)) then
+	p.TurnToSafe(700, Offset + Ori * 400)
+end
+if (c.simulate(p.GoToSafe, 700, Offset + Ori * 400)) then
+	p.GoToSafe(700, Offset + Ori * 400)
+end
 p.GripperMove(true, 95)
 p.GripperMove(false, 95)
-p.GoSafe(-300)
+if (c.simulate(p.GoSafe, -300)) then
+	p.GoSafe(-300)
+end
 p.GripperMove(true, 0)
 p.GripperMove(false, 1)
 p.ClawMove(true, 0)
