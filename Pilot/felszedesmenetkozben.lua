8a79933d71e046461ff941262ef1262393acdfd1
Agoston Feher <feher.agoston@gmail.com>
2012-05-15 15:29:05 +0200

    felszedes menetkozben

---
+++ Pilot/felszedesmenetkozben.lua
@@ -0,0 +1,42 @@
+dofile("Pilot/calibration.lua")
+
+dofile("Pilot/functions.lua")
+
+p.Go(30)
+
+p.MoveToSafe(250, 500)
+
+p.TurnToSafe(250, 1500)
+
+p.runparallel(
+	function()
+		p.GripperMove(Right, 90)
+		p.GripperMove(Left, 90)
+	end,
+	function()
+		p.ClawMove(Right, 90)
+		p.ClawMove(Left, 90)
+	end
+)
+p.ArmMove(90)
+
+p.runparallel(
+	function()
+		p.GoToSafe(250, 2500)
+	end,
+	function()
+		p.Compressor(true)
+		repeat
+			x, y, phi = c.GetRobotPos()
+			p.process()
+		until (y > 720)
+		p.ArmMove(100)
+		p.sleep(200)
+		p.ArmMove(10, 1000, 200)
+		p.Valve(true)
+		p.sleep(100)
+		p.Valve(false)
+		p.ArmMove(0)
+		p.Compressor(false)
+	end
+)
