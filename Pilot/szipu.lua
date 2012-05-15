24f47755bd4d1862e421ff4bc53b147156925a11
Agoston Feher <feher.agoston@gmail.com>
2012-05-15 21:16:46 +0200

    szipunal emeles lassabban, kompresszor kikapcsolas

--- Pilot/szipu.lua
+++ Pilot/szipu.lua
@@ -1,11 +1,15 @@
 c.SetMotorSupply(true)
 
 p.sleep(1000)
+
 p.Compressor(true)
+p.sleep(200)
 p.ArmMove(100)
 p.sleep(200)
-p.ArmMove(10)
+p.ArmMove(10, 1000, 200)
 p.Valve(true)
 p.sleep(300)
 p.Valve(false)
 p.ArmMove(45)
+p.Compressor(false)
+
