05ff5ef6efc9e22ddfeb747b7e2e16ad35bc7e9c
Agoston Feher <feher.agoston@gmail.com>
2012-05-15 21:05:29 +0200

    Felszedes es Urites

--- Pilot/functions.lua
+++ Pilot/functions.lua
@@ -1,16 +1,42 @@
 
 -- felszedes karral
-function PickupWithArm()
+function Felszedes(waitforit)
 	p.Compressor(true)
 	p.sleep(200)
-	p.ArmMove(100)
+	p.ArmMove(105)
 	p.sleep(200)
 	p.ArmMove(10, 1000, 200)
 	p.Valve(true)
-	p.sleep(100)
-	p.Valve(false)
-	p.ArmMove(0)
-	p.Compressor(false)
+	p.sleep(150)
+	c.ArmMove(0)
+	c.Valve(false)
+	c.Compressor(false)
+	if (waitforit) then
+		while (control.CompressorInProgress()) do
+			process()
+		end
+		while (control.ValveInProgress()) do
+			process()
+		end
+		while (control.ArmMoveInProgress()) do
+			process()
+		end
+	end
+end
+
+-- urites
+function Urites(waitforit)
+	-- Urites
+	p.ConsoleMove(140, 1000, 15)
+	
+	-- Vissza
+	p.ConsoleMove(20, 400, 15)
+	c.CalibrateConsole()
+	if (waitforit) then
+		while (control.CalibrateConsoleInProgress()) do
+			process()
+		end
+	end
 end
 
 -- Beszorulas feloldasa
