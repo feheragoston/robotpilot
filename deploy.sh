96f9854ab6bebdf9260a2b465bc81a729baa9ce2
Agoston Feher <feher.agoston@gmail.com>
2012-05-19 16:45:18 +0200

    deployba s3

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ scp Debug32bit/robotpilot robot@geode:
 scp Pilot/pilot.lua robot@geode:Pilot
 scp Pilot/calibration.lua robot@geode:Pilot
 scp Pilot/functions.lua robot@geode:Pilot
+scp Pilot/s3.lua robot@geode:Pilot
 if [ -f "$1" ] ; then
 	scp $1 robot@geode:$1
 fi
